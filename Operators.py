8a76e286ee1dfd078569cd13e3ca785ad79800ed
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:55:19 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,35 @@
+print('21 Dec 2022')
+
+print("Operators \n Arithmetic")
+a=5
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+print('Assignment ')
+
+print(a)
+a   =10
+print(a)
+a+=5
+a= a+5
+print(a)
+a-=3
+print(a)
+
+
+a=100
+b=90
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
